33394a2f5b9c75d0347241a2c4abd64ea805191d
Tero Parviainen <tero@teropa.info>
2015-08-30 17:10:37 +0300

    immutable trees test

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,4 +1,4 @@
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 import {expect} from 'chai';
 
 describe('immutability', () => {
@@ -38,6 +38,35 @@ describe('immutability', () => {
         'Trainspotting',
         '28 Days Later'
       ));
+    });
+
+  });
+
+  describe('trees', () => {
+
+    function addMovie(currentState, movie) {
+      return currentState.update('movies', movies => movies.push(movie));
+    }
+
+    it('are immutable', () => {
+      let state = Map({
+        movies: List.of('Trainspotting', '28 Days Later')
+      });
+      let nextState = addMovie(state, 'Sunshine');
+
+      expect(nextState).to.equal(Map({
+        movies: List.of(
+          'Trainspotting',
+          '28 Days Later',
+          'Sunshine'
+        )
+      }));
+      expect(state).to.equal(Map({
+        movies: List.of(
+          'Trainspotting',
+          '28 Days Later'
+        )
+      }));
     });
 
   });
