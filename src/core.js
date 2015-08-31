944c7e26afb3dca7b6fcc17c0fa0cb5acfc5119d
Tero Parviainen <tero@teropa.info>
2015-08-31 06:48:43 +0300

    logic for moving to next vote

--- src/core.js
+++ src/core.js
@@ -4,8 +4,19 @@ export function setEntries(state, entries) {
   return state.set('entries', List(entries));
 }
 
+function getWinners(vote) {
+  if (!vote) return [];
+  const [one, two] = vote.get('pair');
+  const oneVotes = vote.getIn(['tally', one], 0);
+  const twoVotes = vote.getIn(['tally', two], 0);
+  if      (oneVotes > twoVotes)  return [one];
+  else if (oneVotes < twoVotes)  return [two];
+  else                           return [one, two];
+}
+
 export function next(state) {
-  const entries = state.get('entries');
+  const entries = state.get('entries')
+                       .concat(getWinners(state.get('vote')));
   return state.merge({
     vote: Map({pair: entries.take(2)}),
     entries: entries.skip(2)
--- test/core_spec.js
+++ test/core_spec.js
@@ -42,6 +42,50 @@ describe('application logic', () => {
       }));
     });
 
+    it('puts winner of current vote back to entries', () => {
+      expect(
+        next(Map({
+          vote: Map({
+            pair: List.of('Trainspotting', '28 Days Later'),
+            tally: Map({
+              'Trainspotting': 4,
+              '28 Days Later': 2
+            })
+          }),
+          entries: List.of('Sunshine', 'Millions', '127 Hours')
+        }))
+      ).to.equal(
+        Map({
+          vote: Map({
+            pair: List.of('Sunshine', 'Millions')
+          }),
+          entries: List.of('127 Hours', 'Trainspotting')
+        })
+      );
+    });
+
+    it('puts both from tied vote back to entries', () => {
+      expect(
+        next(Map({
+          vote: Map({
+            pair: List.of('Trainspotting', '28 Days Later'),
+            tally: Map({
+              'Trainspotting': 3,
+              '28 Days Later': 3
+            })
+          }),
+          entries: List.of('Sunshine', 'Millions', '127 Hours')
+        }))
+      ).to.equal(
+        Map({
+          vote: Map({
+            pair: List.of('Sunshine', 'Millions')
+          }),
+          entries: List.of('127 Hours', 'Trainspotting', '28 Days Later')
+        })
+      );
+    });
+
   });
 
   describe('vote', () => {
