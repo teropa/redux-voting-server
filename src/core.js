05cd8ad6b590c582d9c521a717b1f1bd635c6965
Tero Parviainen <tero@teropa.info>
2015-08-31 06:31:53 +0300

    logic for vote

--- src/core.js
+++ src/core.js
@@ -11,3 +11,11 @@ export function next(state) {
     entries: entries.skip(2)
   });
 }
+
+export function vote(state, entry) {
+  return state.updateIn(
+    ['vote', 'tally', entry],
+    0,
+    tally => tally + 1
+  );
+}
--- test/core_spec.js
+++ test/core_spec.js
@@ -1,7 +1,7 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
 
-import {setEntries, next} from '../src/core';
+import {setEntries, next, vote} from '../src/core';
 
 describe('application logic', () => {
 
@@ -40,6 +40,57 @@ describe('application logic', () => {
         }),
         entries: List.of('Sunshine')
       }));
+    });
+
+  });
+
+  describe('vote', () => {
+
+    it('creates a tally for the voted entry', () => {
+      expect(
+        vote(Map({
+          vote: Map({
+            pair: List.of('Trainspotting', '28 Days Later')
+          }),
+          entries: List()
+        }), 'Trainspotting')
+      ).to.equal(
+        Map({
+          vote: Map({
+            pair: List.of('Trainspotting', '28 Days Later'),
+            tally: Map({
+              'Trainspotting': 1
+            })
+          }),
+          entries: List()
+        })
+      );
+    });
+
+    it('adds to existing tally for the voted entry', () => {
+      expect(
+        vote(Map({
+          vote: Map({
+            pair: List.of('Trainspotting', '28 Days Later'),
+            tally: Map({
+              'Trainspotting': 3,
+              '28 Days Later': 2
+            })
+          }),
+          entries: List()
+        }), 'Trainspotting')
+      ).to.equal(
+        Map({
+          vote: Map({
+            pair: List.of('Trainspotting', '28 Days Later'),
+            tally: Map({
+              'Trainspotting': 4,
+              '28 Days Later': 2
+            })
+          }),
+          entries: List()
+        })
+      );
     });
 
   });
