97416afa0a829ce1cd28ca1f280e2dc842652529
Tero Parviainen <tero@teropa.info>
2015-09-05 11:34:46 +0300

    load example entries

---
+++ entries.json
@@ -0,0 +1,13 @@
+[
+  "Shallow Grave",
+  "Trainspotting",
+  "A Life Less Ordinary",
+  "The Beach",
+  "28 Days Later",
+  "Millions",
+  "Sunshine",
+  "Slumdog Millionaire",
+  "127 Hours",
+  "Trance",
+  "Steve Jobs"
+]
--- index.js
+++ index.js
@@ -4,3 +4,8 @@ import {startServer} from './src/server';
 export const store = makeStore();
 startServer(store);
 
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries: require('./entries.json')
+});
+store.dispatch({type: 'NEXT'});
