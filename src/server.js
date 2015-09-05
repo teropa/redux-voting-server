add532116ef0b860fce6becbbb169140ea1d1f96
Tero Parviainen <tero@teropa.info>
2015-09-05 11:34:28 +0300

    receive remote redux actions

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ export function startServer(store) {
 
   io.on('connection', (socket) => {
     socket.emit('state', store.getState().toJS());
+    socket.on('action', store.dispatch.bind(store));
   });
 
 }
